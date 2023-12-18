702b83d83056155de0afcb114c0a8d9db2d1ad88
krarlos <carlosramirez.artigas@gmail.com>
2023-12-18 20:14:17 -0300

    prueba

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { BrowserRouter, Route, Router } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Router>
+        <Route />
+      </Router>
+    </BrowserRouter>
+  );
+}
+
+export default App;
